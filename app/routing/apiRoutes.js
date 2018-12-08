b8e430e8f03b90e654eec538b0f51c292411d354
y4donts4y <stella.l.griffith@gmail.com>
2018-12-07 23:58:57 -0500

    wrapping up for the night

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,13 @@
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 // * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
+// Gets data from friends.js file
+
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
 });
@@ -10,5 +17,7 @@ app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
     console.log(newFriend);
-    res.json(newFriends);
+    res.json(friends);
 });
+
+};
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
